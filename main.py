89d7a64bb807cb0272a8a9467640fb1be3723eb5
Stefan Moosbrugger <stefanmoosbrugger@gmail.com>
2022-02-03 18:45:50 +0100

    finalize mongodb support

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from utils.database import *
 
 c = Connection.get_connection(ConnectionType.Default)
 d = Database.get_database(DatabaseType.MongoDb)
+d.open("51.68.5.233","27017","stationdb")
 
 # get all weather data for switzerland region
-#proc_ch = Processor.get_processor(Region.Schweiz,c)
-#stations_ch = proc_ch.get_stations()
-#for station_ch in stations_ch:
-#   proc_ch.get_data_for(station_ch)
-#   print(station_ch)
+proc_ch = Processor.get_processor(Region.Schweiz,c)
+stations_ch = proc_ch.get_stations()
+for station_ch in stations_ch:
+   proc_ch.get_data_for(station_ch)
+   print("write "+station_ch.name)
+   d.write_station_data(station_ch)
 
 # get all weather data for bavaria region
-#proc_by = Processor.get_processor(Region.Bayern,c)
-#stations_by = proc_by.get_stations()
-#for station_by in stations_by:
-#   proc_by.get_data_for(station_by)
-#   print(station_by)
+proc_by = Processor.get_processor(Region.Bayern,c)
+stations_by = proc_by.get_stations()
+for station_by in stations_by:
+   proc_by.get_data_for(station_by)
+   print("write "+station_by.name)
+   d.write_station_data(station_by)
 
 # get all weather data for tirol region
 proc_ti = Processor.get_processor(Region.Tirol,c)
 stations_ti = proc_ti.get_stations()
 for station_ti in stations_ti:
    proc_ti.get_data_for(station_ti)
-d.open("51.68.5.233","27017","stationdb")
-print(stations_ti[20].meta_as_dict())
-print(stations_ti[20].measurements_as_list())
-d.write_station_data(stations_ti[20])
-d.get_station_data(stations_ti[20])
+   print("write "+station_ti.name)
+   d.write_station_data(station_ti)
--- utils/mongodb.py
+++ utils/mongodb.py
@@ -5,28 +5,29 @@ class MongoDbClient:
         self.client = pymongo.MongoClient(str("mongodb://"+str(host)+":"+str(port)+"/"))
         self.db = self.client[str(db)]
         self.stations = self.db["stations"]
+        self.measurements = self.db["measurements"]
 
     def write_station_data(self,station):
         dbStation = { "name": station.name, "region": str(station.region) }
         dbObj = 0
-        dbStationQ = self.stations.find(dbStation)
-        if len(list(dbStationQ))==0:
+        if self.stations.count_documents(dbStation)==0:
             # insert station object
-            dbObj = self.stations.insert_one(station.meta_as_dict())
-        else:
-            # use existing
-            dbObj = dbStationQ[0]
+            self.stations.insert_one(station.meta_as_dict())
+        # fetch station
+        dbObj = self.stations.find_one(dbStation)
+        # get all measurements
         mlist = station.measurements_as_list()
         for m in mlist:
-            print(m)
-            #dbObj.update({_id: m.}, { $set: { "data.2" : newValue } });
-            #p = self.stations.find({"some": "condition", "comments.type": "image"})            
+            m["stationId"] = dbObj["_id"]
+            self.measurements.update_one(
+                {"stationId":dbObj["_id"], "timestamp":m["timestamp"]},
+                {"$set":m},
+                upsert=True)
 
     def get_station_data(self,station):
         dbStation = { "name": station.name, "region": str(station.region) }
-        dbStationQ = self.stations.find(dbStation)
-        if len(dbStationQ)==0:
-            return ""
+        if self.stations.count_documents(dbStation):
+            dbObj = self.stations.find_one(dbStation)
+            return list(self.measurements.find({"stationId":dbObj["_id"]}))
         else:
-            # use existing
-            return dbStationQ[0]
+            return ""
