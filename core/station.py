cc3927a0c635acda0b2a632634d18174e17e20ed
Stefan <stefan@pop-os.localdomain>
2022-11-28 16:43:07 +0100

    add explicit type casts to avoid influxdb issues

--- core/station.py
+++ core/station.py
@@ -2,21 +2,21 @@
 class Station:
     # ctor 
     def __init__(self,sid,name,longi,lat,alt,region):
-        self.id = sid
-        self.name = name
-        self.long = longi
-        self.lat = lat
-        self.altitude = alt
+        self.id = str(sid)
+        self.name = str(name)
+        self.long = round(float(longi),3)
+        self.lat = round(float(lat),3)
+        self.altitude = int(alt)
         self.region = region
         self.data = []
 
     def meta_as_dict(self):
         d = {
-            "internalid": str(self.id),
-            "name": str(self.name),
-            "latitude": str(self.lat),
-            "longitude": str(self.long),
-            "altitude": str(self.altitude),
+            "internalid": self.id,
+            "name": self.name,
+            "latitude": self.lat,
+            "longitude": self.long,
+            "altitude": self.altitude,
             "region": str(self.region)
         }
         return d
